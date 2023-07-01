75466822371d5d617c0b2d423a7d35cd060d4923
AlexandreS10 <alexandres10santos@gmail.com>
2023-07-01 14:08:12 -0300

    funcionalidade total

--- js/home.js
+++ js/home.js
@@ -11,42 +11,56 @@ function renderVideos(categoria, element) {
     const modalVideo = `staticBackdrop${index}`;
 
     html += `
-    
-    <div class="col-3 mb-4">
-        <div class="card border-0" style="width: 18rem;">
-            <div class="zoom-card">
-              <div class="card border-0" data-bs-toggle="modal" onmouseover="showCardBody(this)"
-               onmouseout="hideCardBody(this)">
-                  <img src="${item.img}" class="border-0" style="width:100%">
-              </div>
-            </div>
-            <div class="card-body bg-black border-0" style="display: none;">
-              <button class="btn text-white" data-bs-target="#${modalVideo}"><img src="./images/play-circle.svg">${item.titulo}</button>
-            </div>
-        </div>
-    </div>
-      <div class="modal fade" id="${modalVideo}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-body">
-              <iframe src="${item.link}"></iframe>
-            </div>
-          </div>
+    <div class="col-3 mb-3">
+    <div class="card border-0" style="width: 16rem;">
+      <div class="zoom-card">
+        <div class="card border-0" data-bs-toggle="modal" data-bs-target="#${modalVideo}" onmouseover="showCardBody(this)" onmouseout="hideCardBody(this)">
+          <img src="${item.img}" class="border-0" style="width:100%">
         </div>
       </div>
+      <div class="card-body bg-black border-0" style="display: none;">
+        <button class="btn text-white" data-bs-toggle="modal" data-bs-target="#${modalVideo}"><img src="./images/play-circle.svg">${item.titulo}</button>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade" id="${modalVideo}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div style="position: relative; padding-bottom: 56.25%; height: 0;">
+              <iframe src="${item.link}" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" allowfullscreen></iframe>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
       `;
   });
 
   element.innerHTML += html;
+
+  // Inicialize os modais após adicionar ao DOM
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach((modal) => new bootstrap.Modal(modal));
 }
+
 function showCardBody(element) {
   const cardBody = element.parentNode.nextElementSibling;
-  cardBody.style.display = "block";
+  const isMouseOverCardBody = cardBody.contains(event.relatedTarget);
+  if (!isMouseOverCardBody) {
+    cardBody.style.display = "block";
+  }
 }
 
 function hideCardBody(element) {
   const cardBody = element.parentNode.nextElementSibling;
-  cardBody.style.display = "none";
+  const isMouseOverCardBody = cardBody.contains(event.relatedTarget);
+  if (!isMouseOverCardBody) {
+    cardBody.style.display = "none";
+  }
 }
 renderVideos("podcast", rowVideosGrowcast);
 renderVideos("webinar", rowVideosWebinar);
